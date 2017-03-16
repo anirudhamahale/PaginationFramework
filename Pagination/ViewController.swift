f3fe587a5e19ba67c84203b8bbf0806c3b117e9a
Anirudha Mahale <anirudhamahale@gmail.com>
2017-03-16 12:37:45 +0530

    Initial Commit

---
+++ Pagination/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pagination
+//
+//  Created by LA Argon on 16/03/17.
+//  Copyright © 2017 LA Argon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
