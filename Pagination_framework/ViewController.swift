72a5b254fbe961cfc256c475477bb7abd69176fc
Anirudha Mahale <anirudhamahale@gmail.com>
2017-03-17 12:45:56 +0530

    new project.

---
+++ Pagination_framework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pagination_framework
+//
+//  Created by LA Argon on 17/03/17.
+//  Copyright © 2017 LA Argon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
