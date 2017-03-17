819a78227d81d90d7f460fe2e4cad39f79a919b9
Anirudha Mahale <anirudhamahale@gmail.com>
2017-03-17 13:09:25 +0530

    Implemented Alamofire.

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Pagination_framework' do
+	pod 'AlamofireSwiftyJSON'
+end
---
+++ Pods/Target Support Files/Pods-Pagination_framework/Pods-Pagination_framework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pagination_framework : NSObject
+@end
+@implementation PodsDummy_Pods_Pagination_framework
+@end
