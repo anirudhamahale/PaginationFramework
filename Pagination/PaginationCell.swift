659c5b19e045fc80c48c7a70a38a3764c62f7d12
Anirudha Mahale <anirudhamahale@gmail.com>
2017-03-17 11:20:16 +0530

    Pushing before implementing view beneath tableview.

---
+++ Pagination/PaginationCell.swift
@@ -0,0 +1,15 @@
+
+//
+//  PaginationCell.swift
+//  Pagination
+//
+//  Created by LA Argon on 16/03/17.
+//  Copyright © 2017 LA Argon. All rights reserved.
+//
+
+import UIKit
+
+class PaginationCell: UITableViewCell {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
--- Pagination/ViewController.swift
+++ Pagination/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var count = 20
+    
+    var isPaginating = false {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Check if the scroll is bottom of the table
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let offset = scrollView.contentOffset
+        let bounds = scrollView.bounds
+        let size = scrollView.contentSize
+        let inset = scrollView.contentInset
+        let y = offset.y + bounds.size.height - inset.bottom
+        let h = size.height
+        
+        let reload_distance: CGFloat = -30
+        
+        if y > h + reload_distance {
+            if !isPaginating {
+                isPaginating = true
+                let triggerTime = (Int64(NSEC_PER_SEC) * Int64(5))
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, triggerTime), dispatch_get_main_queue(), { () -> Void in
+                    self.isPaginating = false
+                })
+            }
+        }
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return isPaginating ? 2 : 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return count
+        default:
+            return 1
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+            cell.textLabel?.text = "\(indexPath.row)"
+            return cell
+        default:
+            let cell = tableView.dequeueReusableCellWithIdentifier("PaginationCell", forIndexPath: indexPath) as! PaginationCell
+            cell.activityIndicator.startAnimating()
+            return cell
+        }
+    }
+}
