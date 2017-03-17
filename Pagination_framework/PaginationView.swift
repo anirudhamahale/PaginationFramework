b39d34c73ec5c76995ec2c01fe704d5bfb7b1364
Anirudha Mahale <anirudhamahale@gmail.com>
2017-03-17 13:11:59 +0530

    New file added.

---
+++ Pagination_framework/PaginationView.swift
@@ -0,0 +1,14 @@
+//
+//  PaginationView.swift
+//  Pagination_framework
+//
+//  Created by LA Argon on 17/03/17.
+//  Copyright © 2017 LA Argon. All rights reserved.
+//
+
+import UIKit
+
+class PaginationView: UIView {
+
+    
+}
